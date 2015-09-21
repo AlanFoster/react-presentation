9d97536a0122d8993cdb79e9859ca81c7a9d9f24
Alan Foster <alan@alanfoster.me>
2015-09-22 00:50:38 +0100

    Add component types information

--- presentation/deck.jsx
+++ presentation/deck.jsx
@@ -29,10 +29,10 @@ export default class extends React.Component {
         </Slide>
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
           <List>
-            <ListItem><Appear fid="1">Created by Facebook</Appear></ListItem>
-            <ListItem><Appear fid="2">JavaScript framework for front-end Components</Appear></ListItem>
-            <ListItem><Appear fid="3">Opinionated on Data Flow</Appear></ListItem>
-            <ListItem><Appear fid="4">Fast - Virtual DOM</Appear></ListItem>
+            <ListItem><Appear>JavaScript framework for front-end Components</Appear></ListItem>
+            <ListItem><Appear>Opinionated on Data Flow</Appear></ListItem>
+            <ListItem><Appear>Fast - Virtual DOM</Appear></ListItem>
+            <ListItem><Appear>Created by Facebook</Appear></ListItem>
           </List>
         </Slide>
 
@@ -55,16 +55,16 @@ export default class extends React.Component {
         </Slide>
 
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <Appear fid="1">
+          <Appear>
             <Heading textColor="primary" >
               Props
             </Heading>
           </Appear>
 
           <List textColor="primary">
-            <ListItem><Appear fid="2">Passed to the component via attributes</Appear></ListItem>
-            <ListItem><Appear fid="3">Immutable - The component will not change these values</Appear></ListItem>
-            <ListItem><Appear fid="4">Changing props causes a re-render</Appear></ListItem>
+            <ListItem><Appear>Passed to the component via attributes</Appear></ListItem>
+            <ListItem><Appear>Immutable - The component will not change these values</Appear></ListItem>
+            <ListItem><Appear>Changing props causes a re-render</Appear></ListItem>
           </List>
         </Slide>
 
@@ -76,16 +76,16 @@ export default class extends React.Component {
         </Slide>
 
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <Appear fid="1">
+          <Appear>
             <Heading textColor="primary" >
               State
             </Heading>
           </Appear>
 
           <List textColor="primary">
-            <ListItem><Appear fid="2">Private - Maintained Internally</Appear></ListItem>
-            <ListItem><Appear fid="3">Mutable - The component <strong>can</strong> change these values</Appear></ListItem>
-            <ListItem><Appear fid="4">Changing state causes a re-render</Appear></ListItem>
+            <ListItem><Appear>Private - Maintained Internally</Appear></ListItem>
+            <ListItem><Appear>Mutable - The component <strong>can</strong> change these values</Appear></ListItem>
+            <ListItem><Appear>Changing state causes a re-render</Appear></ListItem>
           </List>
         </Slide>
 
@@ -95,6 +95,93 @@ export default class extends React.Component {
             source={require("raw!./examples/state.example")}
             margin="20px auto"/>
         </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Appear>
+            <Heading textColor="primary" textFont="secondary">
+              Component Types
+            </Heading>
+          </Appear>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Appear>
+            <Heading textColor="primary" >
+              Pure Components
+            </Heading>
+          </Appear>
+
+          <List textColor="primary">
+            <ListItem><Appear>Relies only on props</Appear></ListItem>
+            <ListItem><Appear>Contains no state</Appear></ListItem>
+            <ListItem><Appear>Renders the same way given the same props (idempotency)</Appear></ListItem>
+            <ListItem><Appear>Easier to test, as all data is external and determinstic</Appear></ListItem>
+            <ListItem><Appear>Re-usable, not dependant on specific data-sources</Appear></ListItem>
+            <ListItem><Appear>ie the Person example</Appear></ListItem>
+          </List>
+        </Slide>
+
+        <Slide transition={['zoom', 'fade']} bgColor="primary">
+          <CodePane
+            lang="javascript"
+            source={require("raw!./examples/person.example")}
+            margin="20px auto"/>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Appear>
+            <Heading textColor="primary" >
+              Composite Components
+            </Heading>
+          </Appear>
+
+          <List textColor="primary">
+            <ListItem><Appear>Groups together Pure Components</Appear></ListItem>
+            <ListItem><Appear>Maintains the state required to populate pure components</Appear></ListItem>
+            <ListItem><Appear>Spoilers: Useful for flux pattern</Appear></ListItem>
+          </List>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <CodeDemo
+            lang="javascript"
+            source={require("raw!./examples/composite.example")}
+            margin="20px auto"/>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Image src={images.reactjsLogo.replace('/','')} margin="0px auto 40px" height="293px"/>
+
+          <Heading size={1} fit caps lineHeight={1} textColor="primary">
+            React.js - Real World
+          </Heading>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Appear>
+            <Heading textColor="primary">
+              What do we need?
+            </Heading>
+          </Appear>
+
+          <Appear>
+            <List>
+              <ListItem><Appear>Opinionated Folder Structure</Appear></ListItem>
+              <ListItem><Appear>Data Management - Flux</Appear></ListItem>
+              <ListItem><Appear>CSS Styling</Appear></ListItem>
+              <ListItem><Appear>Testing</Appear></ListItem>
+              <ListItem><Appear>Building</Appear></ListItem>
+              <ListItem><Appear>Linting</Appear></ListItem>
+            </List>
+          </Appear>
+        </Slide>
+
+        {/*
+          TODO:
+            Prop Types
+            Smaller differences, gotchas - class vs className, for htmlFor - https://facebook.github.io/react/docs/tags-and-attributes.html
+            ES6 Features -  binding arrow
+        */}
       </Deck>
     );
   }
